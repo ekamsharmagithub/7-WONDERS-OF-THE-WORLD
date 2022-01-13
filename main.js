70714762891a127d920522ded5757911953703f4
ekamsharmagithub <90507473+ekamsharmagithub@users.noreply.github.com>
2022-01-13 22:06:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+user_name = localStorage.getItem("Username");
+
+document.getElementById("welcome_user_name").innerHTML = "Welcome " + user_name + "!";
+  
+  function one() {
+    window.alert(
+        "The Great Wall of China is an ancient wall in China. ... It was finished in 1878 and it was meant to protect the north of the empire of China from enemy attacks. It is the longest structure humans have ever built. It is about 21,196 kilometres (13,171 miles) long, 9.1 metres (30 feet) wide and 15 metres (50 feet) high."
+    );
+  }
+  function two() {
+    window.alert(
+        "Christ the Redeemer, Portuguese Cristo Redentor, colossal statue of Jesus Christ at the summit of Mount Corcovado, Rio de Janeiro, southeastern Brazil. ... The statue of Christ the Redeemer was completed in 1931 and stands 98 feet (30 metres) tall, its horizontally outstretched arms spanning 92 feet (28 metres)."
+
+    );
+  }
+  function three() {
+    window.alert(
+        "Machu Picchu is an Incan citadel set high in the Andes Mountains in Peru, above the Urubamba River valley. Built in the 15th century and later abandoned, it's renowned for its sophisticated dry-stone walls that fuse huge blocks without the use of mortar, intriguing buildings that play on astronomical alignments and panoramic views. Its exact former use remains a mystery. ―  Address: 08680, Peru Elevation: 2,430 m Area: 325.9 km² Abandoned: 1572 Founded: c. 1450 Region: Latin America and the Caribbean"
+    );
+  }
+  function four() {
+    window.alert(
+        "Chichén Itzá is a complex of Mayan ruins on Mexico's Yucatán Peninsula. A massive step pyramid, known as El Castillo or Temple of Kukulcan, dominates the ancient city, which thrived from around 600 A.D. to the 1200s. Graphic stone carvings survive at structures like the ball court, Temple of the Warriors and the Wall of the Skulls. Nightly sound-and-light shows illuminate the buildings' sophisticated geometry."
+
+    );
+  }
+  function five() {
+    window.alert(
+        "The Colosseum, is an oval amphitheatre in the centre of the city of Rome, Italy, just east of the Roman Forum and is the largest ancient amphitheatre ever built, and is still the largest standing amphitheater in the world today, despite its age."
+    );
+  }
+  function six() {
+    window.alert(
+        "The Taj Mahal is an ivory-white marble mausoleum on the southern bank of the river Yamuna in the Indian city of Agra. It was commissioned in 1632 by the Mughal emperor Shah Jahan to house the tomb of his favourite wife, Mumtaz Mahal; it also houses the tomb of Shah Jahan himself."
+    );
+  }
+  function seven() {
+    window.alert(
+        "Famous for its rock-cut architecture and water conduit system, Petra is also called the Rose City because of the colour of the stone from which it is carved. It has been a UNESCO World Heritage Site since 1985. UNESCO has described Petra as one of the most precious cultural properties of man's cultural heritage. It is built by Nabateans."
+    );
+  }
